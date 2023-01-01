bfeb2238dc3b8a8bd470e24a0145d742687c138b
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-01-01 13:30:16 +0900

    added pages and layouts

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>welcome to the homepage</h1>
+    <AppAlert>
+      てすとだよ
+    </AppAlert>
+  </div>
+</template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute()
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
+
+<template>
+  <section>
+     Counter: {{ counter }}
+    <p>post page</p>
+    <p>{{route.params.id}}</p>
+  </section>
+</template>
+
