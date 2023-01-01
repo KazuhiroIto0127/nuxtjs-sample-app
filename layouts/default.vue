96e5be568a7082d41b292f7e33485b2a5237e16c
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2023-01-01 18:03:04 +0900

    added favicon.ico

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
     <header class="bg-gray-100">
       <nav>
         <ul class="flex gap-3 h-8 justify-center items-center">
+          <img src="../assets/img/profile.png" class="w-10" />
           <li><NuxtLink to="/" class="p-3 hover:bg-white">Home</NuxtLink></li>
           <li><NuxtLink to="/about" class="p-3 hover:bg-white">About</NuxtLink></li>
           <li><NuxtLink to="/posts/1" class="p-3 hover:bg-white">Post 1</NuxtLink></li>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Kazuhiro Ito Nuxt Test App",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.ico" },
+      ],
+    },
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
